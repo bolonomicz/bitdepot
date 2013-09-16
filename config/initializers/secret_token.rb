bfa76e83482678df6dd9bd0b2ece6267ca5d4096
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-16 14:47:53 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitdepot::Application.config.secret_token = '62bfd7888fd5940743b041daba6c0d5d5ad4924d54a99fbb0996875796d9a0a26876f0e701b0bfcb15920f4b97443253b88e696cfcf4322fde41146037e418b0'
